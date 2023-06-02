2551a0117853e33647bd27dae01b0ff7749d4587
Abdullah Essam <abdoessam.2010@gmail.com>
2023-06-02 17:14:34 +0300

    create seeder for super user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +16,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::updateOrCreate([
+            'email'     => 'mohamed@gmail.com',
+            'username'  => 'admin'
+        ], [
+            'f_name'    => 'Mohamed',
+            'l_name'    => 'Ashmawy',
+            'email'     => 'mohamed@gmail.com',
+            'username'  => 'admin',
+            'password'  => Hash::make('Secret123'),
+            'abilities' => '*'
+        ]);
     }
 }
