6c592d10aa866db20c3fe0d6878d89db4a77c4d8
Abdullah Essam <abdoessam.2010@gmail.com>
2023-06-03 00:06:14 +0300

    user abilities created

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,43 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    /***** START ABILITIES *****/
+    CONST ABILITIES = [
+        // Users
+        'users:index',
+        'users:create',
+        'users:update',
+        'users:delete',
+        // Imports
+        'imports:index',
+        'imports:create',
+        'imports:update',
+        'imports:delete',
+        // Exports
+        'exports:index',
+        'exports:create',
+        'exports:update',
+        'exports:delete',
+    ];
+
+    // Users
+    CONST ABILITY_USERS_INDEX   = 'users:index';
+    CONST ABILITY_USERS_CREATE  = 'users:create';
+    CONST ABILITY_USERS_UPDATE  = 'users:update';
+    CONST ABILITY_USERS_DELETE  = 'users:delete';
+    // Imports
+    CONST ABILITY_IMPORTS_INDEX   = 'imports:index';
+    CONST ABILITY_IMPORTS_CREATE  = 'imports:create';
+    CONST ABILITY_IMPORTS_UPDATE  = 'imports:update';
+    CONST ABILITY_IMPORTS_DELETE  = 'imports:delete';
+    // Exports
+    CONST ABILITY_EXPORTS_INDEX   = 'exports:index';
+    CONST ABILITY_EXPORTS_CREATE  = 'exports:create';
+    CONST ABILITY_EXPORTS_UPDATE  = 'exports:update';
+    CONST ABILITY_EXPORTS_DELETE  = 'exports:delete';
+
+    /***** START ABILITIES *****/
+
     /**
      * The attributes that are mass assignable.
      *
