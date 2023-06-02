faf92b9233f14891082854e96e577451761bffd5
Abdullah Essam <abdoessam.2010@gmail.com>
2023-06-02 15:02:14 +0300

    change mix() function to asset() because of fix server public path error

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -43,6 +43,6 @@ body * { text-align: center }
 <body>
     <div id="app"></div>
 
-    <script src="{{ mix('js/app.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
