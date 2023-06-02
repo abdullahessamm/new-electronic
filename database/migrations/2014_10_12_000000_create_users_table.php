f71a45a14ae2ead23d63bcf547e126b9d12a300b
Abdullah Essam <abdoessam.2010@gmail.com>
2023-06-02 03:17:45 +0300

    init migrations, models and sanctum auth driver

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,13 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->smallInteger('id', true, true);
+            $table->string('f_name', 50);
+            $table->string('l_name', 50);
+            $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('abilities');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2023_06_01_231945_create_imports_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('imports', function (Blueprint $table) {
+            $table->id();
+            $table->smallInteger('user_id')->unsigned()->nullable();
+            $table->string('customer_name', 70);
+            $table->float('cost');
+            $table->string('comment')->nullable();
+            $table->foreign('user_id')->references('id')
+            ->on('users')
+            ->onUpdate('cascade')
+            ->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('imports');
+    }
+};
