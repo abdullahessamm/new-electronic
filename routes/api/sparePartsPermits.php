254e37c217adc111bfd9898a449f8e40bb9b9581
Abdullah Essam <abdoessam.2010@gmail.com>
2025-09-20 01:05:37 +0300

    init commit

--- routes/api/sparePartsPermits.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', 'index');
-Route::post('/', 'store');
-Route::patch('/{id}', 'update')->where('id', '^[0-9]+$');
-Route::delete('/{id}', 'destroy')->where('id', '^[0-9]+$');
