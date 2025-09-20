367d4af09ecc7058b7e27ea35867f332a5ceb7ae
Abdullah Essam <abdoessam.2010@gmail.com>
2025-09-20 03:13:40 +0300

    add ui assets files

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <link rel="icon" href="/favicon.ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>نيو إالكترونيك</title>
+    <script type="module" crossorigin src="/assets/dashboard/index-549cefdd.js"></script>
+    <link rel="stylesheet" href="/assets/dashboard/index-13d65533.css">
+  </head>
+  <body>
+    <div id="app"></div>
+    
+  </body>
+</html>
