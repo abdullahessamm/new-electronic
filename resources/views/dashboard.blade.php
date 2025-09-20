8c883803b62a2e98ea5ae32c591a8b1a313ac2b9
Abdullah Essam <abdoessam.2010@gmail.com>
2025-09-20 03:45:02 +0300

    add ui assets files for october

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
     <link rel="icon" href="/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>نيو إالكترونيك</title>
-    <script type="module" crossorigin src="/assets/dashboard/index-d7ffff10.js"></script>
+    <script type="module" crossorigin src="/assets/dashboard/index-92b4f33c.js"></script>
     <link rel="stylesheet" href="/assets/dashboard/index-13d65533.css">
   </head>
   <body>
